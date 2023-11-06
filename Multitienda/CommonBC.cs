14c812d69c5b84380a1c643b91c003aaf765d25c
Erick <ekmoya@proton.me>
2023-11-06 02:59:36 +0000

    [NH] :hammer: Working project

---
+++ Multitienda/CommonBC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multitienda.Negocio
+{
+    public class CommonBC
+    {
+        private static MultitiendaEntities _modeloMultitienda;
+
+        public static MultitiendaEntities ModeloMultitienda
+        {
+            get
+            {
+                if (_modeloMultitienda == null)
+                {
+                    _modeloMultitienda = new MultitiendaEntities();
+                }
+                return _modeloMultitienda;
+            }
+        }
+
+        public CommonBC()
+        {
+
+        }
+    }
+}
